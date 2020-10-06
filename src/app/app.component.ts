451759bae5929a1c43eaa0600ce39d507956ceb0
sairam <m.sairam14021998@gmail.com>
2020-10-06 20:02:53 +0530

    parent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit} from '@angular/core';
+import { AfterViewInit, Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterViewInit {
   constructor() { }
+  message = 'Nothing pretty new message'
+  arr = [1,2,3,4,5,6,7,8,9,10]
+  data;
+  name = "sairam"
 
+  ngOnInit(){
+    console.log("hi");
+  }
+  recieveMessage($message){
+    this.message = $message;
+  }
+  ngAfterViewInit() {
 
-  ngOnInit(){}
+  }
 };
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { Empcomp1Component } from './empcomp1/empcomp1.component';
+import { Empcomp2Component } from './empcomp2/empcomp2.component';
+import { Sibling1Component } from './sibling1/sibling1.component';
+import { Sibling2Component } from './sibling2/sibling2.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    Empcomp1Component,
+    Empcomp2Component,
+    Sibling1Component,
+    Sibling2Component,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
