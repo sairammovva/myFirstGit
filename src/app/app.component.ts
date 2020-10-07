c2bb0167d29d1c05a262c2291c19cfa6581177b8
sairam <m.sairam14021998@gmail.com>
2020-10-07 10:24:13 +0530

    monday assignment regarding passing data b/w parent and child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,32 @@ import { AfterViewInit, Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit, AfterViewInit {
   constructor() { }
+  text1 = 'this is text of box 1';
+  text2 = 'this is text of box 2';
+  text3 = 'this is text of box 3';
+  text4 = 'this is text of box 4';
+  text5 = 'this is text of box 5';
+  source1 = "https://rukminim1.flixcart.com/image/150/150/k7285u80/t-shirt/6/e/x/m-maw19cn104b-metronaut-original-imafpdz6n2p89zqg.jpeg?q=70"
+  source2 = "https://rukminim1.flixcart.com/image/150/150/k5lcvbk0pkrrdj/headphone/dynamic/z/j/h/jbl-c150siublk-original-imafeyby52kxehz2.jpeg?q=70"
+  source3 = "https://rukminim1.flixcart.com/image/150/150/kcnp8y80/speaker/home-theatre/l/s/n/a3390h11-soundcore-original-imaftqbpyfrurxnv.jpeg?q=70"
+  source4 = "https://rukminim1.flixcart.com/image/150/150/kbs9k7k0/headphone/c/g/t/earbuds-s-redmi-original-imaftfwfhdygcb2s.jpeg?q=70"
+  /*
   message = 'Nothing pretty new message'
   arr = [1,2,3,4,5,6,7,8,9,10]
   data;
   name = "sairam"
-
+*/
   ngOnInit(){
-    console.log("hi");
+    //console.log("hi");
   }
+  /*
   recieveMessage($message){
     this.message = $message;
   }
+  */
   ngAfterViewInit() {
 
   }
-};
+
+}
 
